00a0f62f1fab3b96913bf7c9d37ec614fa37848e
Gearhart_Ivy <geari789@hsd.k12.or.us>
2018-10-17 15:28:57 -0700

    adding player in platform objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody m_rb;
+	// Use this for initialization
+	void Start () {
+        m_rb = GetComponent<Rigidbody>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        float movement = Input.GetAxis("Horizontal");
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+	}
+}
